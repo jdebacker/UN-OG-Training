eaaf2c413e2493443684273712dae54b496e382b
Richard Evans <rickecon@gmail.com>
2023-07-30 22:28:56 -0600

    Added test_add_junk.py

---
+++ tests/test_add_junk.py
@@ -0,0 +1,8 @@
+def test_add_junk(arg1=2, arg2=3):
+    """
+    This is just a fake test of code in the `/code/` directory. We can delete
+    this as soon as we have some real tests.
+    """
+    junk_sum = arg1 + arg2
+
+    assert junk_sum == 5
